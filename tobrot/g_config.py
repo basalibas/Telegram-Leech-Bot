67a6d68e71ecf0b6c48d9b35e17a8e4cb65b28ca
basalibas <73498778+basalibas@users.noreply.github.com>
2020-11-22 23:45:22 +0300

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -7,7 +7,7 @@ class Config(Config):
     API_HASH = "84ce113c503f77e04fcaacc1c0d040f0"
     OWNER_ID = 952345950
     AUTH_CHANNEL = [-442456296]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
+    DESTINATION_FOLDER = "xdxd" #Name of your folder read readme(not id of the folder)
     RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.A0AfH6SMDTkFfSbevvnSIY2jOXPMtnme_hVgh-__tNU-Cc1E7ynfXxeGjlDyO57Sb6p3N5DlnHDMDMflYIrZ0_xuuLpKIkTX6u_oSSiJ-3LPvPeM_Oxey-Tji2_WhXEIaIF7ODdYZNTP-9EUzVDwwicY-H8_hORKta8nMeuJ3GrW2T","token_type":"Bearer","refresh_token":"1//03iKPzSPU0zlNCgYIARAAGAMSNwF-L9IriGbPjI-JRBUSrJD1C0QSCyWmOx78aJdh1Y09OYIF0yH_MpfOgGq8FHah1QaFpU6jAFw","expiry":"2020-11-06T19:58:28.5421305+03:00"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
