e5f6dec7f62db99c4f8982065696db5244485082
basalibas <73498778+basalibas@users.noreply.github.com>
2020-11-22 23:44:39 +0300

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,12 +2,12 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
+    TG_BOT_TOKEN= "1450547115:AAG_pBFawuzb3JHc8l-F2gAqjoKcYBkSdcY"
+    APP_ID = 1893177
+    API_HASH = "84ce113c503f77e04fcaacc1c0d040f0"
+    OWNER_ID = 952345950
+    AUTH_CHANNEL = [-442456296]
     DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.A0AfH6SMDTkFfSbevvnSIY2jOXPMtnme_hVgh-__tNU-Cc1E7ynfXxeGjlDyO57Sb6p3N5DlnHDMDMflYIrZ0_xuuLpKIkTX6u_oSSiJ-3LPvPeM_Oxey-Tji2_WhXEIaIF7ODdYZNTP-9EUzVDwwicY-H8_hORKta8nMeuJ3GrW2T","token_type":"Bearer","refresh_token":"1//03iKPzSPU0zlNCgYIARAAGAMSNwF-L9IriGbPjI-JRBUSrJD1C0QSCyWmOx78aJdh1Y09OYIF0yH_MpfOgGq8FHah1QaFpU6jAFw","expiry":"2020-11-06T19:58:28.5421305+03:00"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
